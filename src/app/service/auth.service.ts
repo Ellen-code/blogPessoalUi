34d9777502fa25b57aabbbce352f671ced6f9923
Ellen-code <ellenprado.pimentel@gmail.com>
2021-06-29 22:29:00 -0300

    Implementação do consumo de rotas

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,24 @@
+
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../model/User';
+import { UserLogin } from '../model/UserLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(userLogin: UserLogin): Observable<UserLogin> {
+    return this.http.post<UserLogin>('https://blogpessoalellen.herokuapp.com/usuarios/logar', userLogin)
+  }
+
+  cadastrar(user: User): Observable<User> {
+    return this.http.post<User>('https://blogpessoalellen.herokuapp.com/usuarios/cadastrar', user)
+  }
+}
